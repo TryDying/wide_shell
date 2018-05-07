b02cb35fe541a668d8ea91a09a74de017e483cb0
trydying <trydying@mail.ustc.edu.cn>
2018-05-07 20:45:00 +0800

    openFoam_flowRatePatch.sh

--- openFoam/flowRatePatch.sh
+++ openFoam/flowRatePatch.sh
@@ -1,2 +1,8 @@
 #!/bin/zsh
+let "simple_time = $1"
+for outlet_num in {1..12}
+do
+    postProcess -func "flowRatePatch(name=outlet${outlet_num})" -time ${simple_time} | sed -n "40p" >> result
+done
 
+    postProcess -func "flowRatePatch(name=inlet)" -time ${simple_time} | sed -n "40p" >> result
