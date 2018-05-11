09f6ed7a9703901a2c39db1a445309931de91d14
trydying <trydying@mail.ustc.edu.cn>
2018-05-11 14:40:20 +0800

    update ofoam.sh ver. 2

--- openFoam/ofoam.sh
+++ openFoam/ofoam.sh
@@ -1,5 +1,5 @@
 #!/bin/zsh
-bash ~/.foamrc
 export FOAM_MESH=$HOME/Documents/0毕设/openfoam/mesh
-export FOAM_CAD=$HOME/Documents/0毕设/openfoam/cad
 export FOAM_REPORT=$HOME/Documents/0毕设/report
+export FOAM_CAD=$HOME/Documents/0毕设/openfoam/cad
+bash ~/.foamrc
